c258195e5505091312dc41ae691bfb6c091d4be5
Rahul Gopal Patil <rahulil@cybage.com>
2022-11-01 17:59:15 +0530

    concept

---
+++ Module/EmailValidator.ts
@@ -0,0 +1,10 @@
+import { Validator as StringValidator } from './Validator';
+
+class EmailValidator implements StringValidator {
+    isValid(s: string): boolean {
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        return emailRegex.test(s);
+    }
+}
+
+export { EmailValidator };
